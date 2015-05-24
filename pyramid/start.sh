42a571151eade39ba140ea965165bd4fa6a95043
pingz <pingz@pingz.info>
2015-05-24 23:10:55 +0800

    ok, this time it must pass.

--- pyramid/start.sh
+++ pyramid/start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # AUTHOR: Zheng, Ping <zp@ping.biz>
 PRJ_DIR=/app/${PRJN}
-[ -d ${PRJ_DIR} ] || pcreate -s starter ${PRJN}
-cd ${PRJ_DIR}
-pserve development.ini --reload
+if [ -d ${PRJ_DIR} ]; then
+    cd ${PRJ_DIR}
+    pserve development.ini --reload
+else
+    pcreate -s starter ${PRJN}
+    cd ${PRJ_DIR}
+    python setup.py develop
+fi
